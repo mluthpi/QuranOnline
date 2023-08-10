d2359722a597286eb975e26a2c6e44654e1d658f
muhammad Luthfiyanto <luthfiyantomuhammad@gmail.com>
2023-08-10 21:47:57 +0700

    update bottom nav

--- app/src/main/java/com/example/quran/BottomNavigationActivity.kt
+++ app/src/main/java/com/example/quran/BottomNavigationActivity.kt
@@ -26,7 +26,7 @@ class BottomNavigationActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_surat, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
